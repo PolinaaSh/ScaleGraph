3690bcb84bd0ac463169ca5ad88966dfbf154975
Полина <paulina.shevlyakova@yandex.ru>
2017-04-07 22:11:29 +0300

    Класс edge

---
+++ ScaleGraph/ScaleGraph/Graph/Edge.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScaleGraph.Graph
+{
+    class Edge
+    {
+        private Node nodeFirst, nodeSecond;//номера вершин, которые соединяет
+        private Color color;
+
+        public Edge(Color color)
+        {
+            this.color = color;
+        }
+
+        public Node NodeFirst
+        {
+            get
+            {
+                return this.nodeFirst;
+            }
+            set
+            {
+                this.nodeFirst = value;
+            }
+        }
+
+        public Node NodeSecond
+        {
+            get
+            {
+                return this.nodeSecond;
+            }
+            set
+            {
+                this.nodeSecond = value;
+            }
+        }
+
+        public Color Color
+        {
+            get
+            {
+                return this.color;
+            }
+            set
+            {
+                this.color = value;
+            }
+        }
+    }
+}
