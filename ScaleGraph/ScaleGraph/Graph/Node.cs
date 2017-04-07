62ec83baca9326231fa8ab496e9ca62eaa6e977a
Полина <paulina.shevlyakova@yandex.ru>
2017-04-07 22:11:04 +0300

    Класс ребра, геттеры, сеттеры

--- ScaleGraph/ScaleGraph/Graph/Node.cs
+++ ScaleGraph/ScaleGraph/Graph/Node.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,80 @@ namespace ScaleGraph.Graph
     class Node
     {
          private int levelVisile;
+         private List<Edge> edges;
+         private Color color;
+         private Point coordinate;
+         private int radius;
 
+
+        public Node(int levelVisible, Color color, Point coordinate, int radius)
+         {
+             this.levelVisile = levelVisible;
+             edges = new List<Edge>();
+             this.color = color;
+             this.coordinate = coordinate;
+             this.radius = radius;
+         }
+
+       public int LevelVisible
+        {
+            get
+            {
+                return this.levelVisile;
+            }
+            set
+            {
+                this.levelVisile = value;
+            }
+        }
+
+       public List<Edge> Edges
+       {
+           get
+           {
+               return this.edges;
+           }
+           set
+           {
+               try
+               {
+                   this.edges = value;
+               }
+               catch (Exception e)
+               {
+                   Console.Write(e.Message);
+               }
+           }
+       }
+
+       public Color Color
+       {
+           get
+           {
+               return this.color;
+           }
+           set
+           {
+               this.color = value;
+           }
+       }
+
+       public Point Coordinate
+       {
+           get
+           {
+               return this.coordinate;
+           }
+           set
+           {
+               this.coordinate = value;
+           }
+       }
+
+       public void AddEdge(Edge edge)
+       {
+           edges.Add(edge);
+       }
 
     }
 }
